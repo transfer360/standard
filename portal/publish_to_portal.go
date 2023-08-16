fead5cf2bbb4c82022451a458492d3b482afbf97
Duane Hodges-Stubbs <dstubbs@solutionlabs.co.uk>
2023-08-16 15:33:01 +0100

    Added Portal

---
+++ portal/publish_to_portal.go
@@ -0,0 +1,51 @@
+package portal
+
+import (
+	"cloud.google.com/go/pubsub"
+	"context"
+	log "github.com/sirupsen/logrus"
+	"strconv"
+)
+
+// SendEvent ------------------------------------------------------------------------------------------
+func SendEvent(ctx context.Context, data []byte, event string, attr map[string]string, ClientID string, ProjectID string) {
+
+	if attr == nil {
+		attr = make(map[string]string)
+	}
+
+	client, err := pubsub.NewClient(ctx, ProjectID)
+	if err != nil {
+		log.Errorf("Unable to publish results: %v", err)
+		return
+	}
+
+	topicName := "save_to_portal"
+	topic := client.Topic(topicName)
+
+	attr["event"] = event
+	attr["client"] = ClientID
+
+	msg := &pubsub.Message{
+		Data:       data,
+		Attributes: attr,
+	}
+
+	if _, err := topic.Publish(ctx, msg).Get(ctx); err != nil {
+		log.Errorf("Unable to publish results: %v", err)
+		return
+	}
+
+}
+
+// SendEventSandBox ------------------------------------------------------------------------------------------------------------------------------------
+func SendEventSandBox(ctx context.Context, data []byte, event string, attr map[string]string, ClientID string, ProjectID string) {
+
+	if attr == nil {
+		attr = make(map[string]string)
+	}
+
+	attr["sandbox"] = strconv.FormatBool(true)
+
+	SendEvent(ctx, data, event, attr, ClientID, ProjectID)
+}
