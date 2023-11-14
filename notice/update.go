c1e42996761e05b8bf48cad228d70e7d7cddba40
Duane Hodges-Stubbs <dstubbs@solutionlabs.co.uk>
2023-11-14 11:10:55 +0000

    Added firestore to TOL Const to status

--- notice/update.go
+++ notice/update.go
@@ -5,32 +5,32 @@ import "time"
 // https://transfer360.dev/#/status/gethirerinformation
 
 type NoticeTOL struct {
-	CompanyName  string `json:"companyname,omitempty"`
-	Name         string `json:"first-name,omitempty"`
-	Surname      string `json:"last-name,omitempty"`
-	AddressLine1 string `json:"addressline1,omitempty"`
-	AddressLine2 string `json:"addressline2,omitempty"`
-	AddressLine3 string `json:"addressline3,omitempty"`
-	AddressLine4 string `json:"addressline4,omitempty"`
-	PostCode     string `json:"post_code,omitempty"`
-	Country      string `json:"country,omitempty"`
-	Primary      bool   `json:"primary"`
+	CompanyName  string `json:"companyname,omitempty" firestore:"CompanyName"`
+	Name         string `json:"first-name,omitempty" firestore:"Name"`
+	Surname      string `json:"last-name,omitempty" firestore:"Surname"`
+	AddressLine1 string `json:"addressline1,omitempty" firestore:"AddressLine1"`
+	AddressLine2 string `json:"addressline2,omitempty" firestore:"AddressLine2"`
+	AddressLine3 string `json:"addressline3,omitempty" firestore:"AddressLine3"`
+	AddressLine4 string `json:"addressline4,omitempty" firestore:"AddressLine4"`
+	PostCode     string `json:"post_code,omitempty" firestore:"PostCode"`
+	Country      string `json:"country,omitempty" firestore:"Country"`
+	Primary      bool   `json:"primary" firestore:"Primary"`
 }
 
 type NoticeUpdateFromLease struct {
-	Status            int       `json:"status"`
-	LeaseCompanyNote  string    `json:"lease_company_note"`
-	ContactInfo       NoticeTOL `json:"contact_information"`
-	ContractNumber    string    `json:"agreement_no,omitempty"`
-	AgreementURL      string    `json:"agreement_url,omitempty"`
-	ContractStartDate string    `json:"hire_from,omitempty"`
-	ContractEndDate   string    `json:"hire_to,omitempty"`
+	Status            int       `json:"status" firestore:"Status"`
+	LeaseCompanyNote  string    `json:"lease_company_note" firestore:"LeaseCompanyNote"`
+	ContactInfo       NoticeTOL `json:"contact_information" firestore:"ContactInfo"`
+	ContractNumber    string    `json:"agreement_no,omitempty" firestore:"ContractNumber"`
+	AgreementURL      string    `json:"agreement_url,omitempty" firestore:"AgreementURL"`
+	ContractStartDate string    `json:"hire_from,omitempty" firestore:"ContractStartDate"`
+	ContractEndDate   string    `json:"hire_to,omitempty" firestore:"ContractEndDate"`
 }
 
 type ParkingChargeNoticeSearchResult struct {
-	FleetID      int                   `json:"fleet_id"`
-	LeaseReturn  NoticeUpdateFromLease `json:"lease_return"`
-	DateOfUpdate time.Time             `json:"date_of_update"`
-	Sref         string                `json:"sref"`
-	ClientID     string                `json:"client_id"`
+	FleetID      int                   `json:"fleet_id" firestore:"FleetID"`
+	LeaseReturn  NoticeUpdateFromLease `json:"lease_return" firestore:"LeaseReturn"`
+	DateOfUpdate time.Time             `json:"date_of_update" firestore:"DateOfUpdate"`
+	Sref         string                `json:"sref" firestore:"Sref"`
+	ClientID     string                `json:"client_id" firestore:"ClientID"`
 }
--- status/status.go
+++ status/status.go
@@ -1,7 +1,10 @@
 package status
 
+import "fmt"
+
 const NOTAHIRERVEHICLE = 0
 const PENDING = 1
+const PENDING_TOL = 2
 const PAID = 3
 const APPEAL = 4
 const NOMINATED = 5
@@ -11,6 +14,7 @@ const REJECTED = 8
 const WAITING_ON_INFO = 9
 const TIME_ELAPSED = 10
 const NO_ANSWER_UNABLE_TO_GET_INFO = 11
+const INFORMATION_REQUIRED = 12
 
 const API_PENDING_STATUS = 0
 const API_NOMINATED_STATUS = 1
@@ -20,12 +24,14 @@ const API_DISMISSED_STATUS = 4
 const API_EVIDENCE_STATUS = 5
 const API_WAITING_STATUS = 6
 const API_PROCESSING = 7
+const API_PENDING_TOL = 8
 
 const API_TIME_ELAPSED = 100
 const API_NO_DATA = 101
 
 const API_ERROR_FATAL_STATUS = 999
 const API_ERROR_RESEND_STATUS = 998
+const API_ERROR_UNKNOWN_STATUS = 997
 
 type ApiStatus struct {
 	Code        int
@@ -37,7 +43,10 @@ func ApiStatusFromLeaseResultStatus(status int) ApiStatus {
 	switch status {
 
 	case PENDING:
-		return ApiStatus{API_PENDING_STATUS, "Pending"}
+		return ApiStatus{API_PENDING_STATUS, "Pending - With lease company"}
+
+	case PENDING_TOL:
+		return ApiStatus{API_PENDING_TOL, "Pending TOL"}
 
 	case PROCESSING:
 		return ApiStatus{API_PROCESSING, "Processing"}
@@ -66,8 +75,11 @@ func ApiStatusFromLeaseResultStatus(status int) ApiStatus {
 	case ERROR:
 		return ApiStatus{API_ERROR_FATAL_STATUS, "Error Fatal"}
 
+	case INFORMATION_REQUIRED:
+		return ApiStatus{API_EVIDENCE_STATUS, "Evidence Required"}
+
 	default:
-		return ApiStatus{status, "Unknown Status"}
+		return ApiStatus{API_ERROR_UNKNOWN_STATUS, fmt.Sprintf("Unknown Status (%d)", status)}
 	}
 
 }
